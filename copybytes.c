6d46ffbc9ffec663f4e3848170581460b553d1af
r cast <157695906+rumbahuh@users.noreply.github.com>
2025-04-04 07:54:47 +0200

    Update copybytes.c
    Comprobacion pensada, lectura de argumentos. To implement cast de char * a int

--- copybytes.c
+++ copybytes.c
@@ -5,14 +5,24 @@
 void
 comprobarpaths(int argc, char *argv[])
 {
+	char *primerpath = argv[1];
+	char *segundopath = argv[2];
+	if (argc == 3) {
+		printf("revisar atoi para cast char * a int)");
+	}
+	
 	printf("function to check paths");
+
+	// if primer path exists
+	//     usar
+	//  default crear o al reves
 }
 
 int
 main(int argc, char *argv[])
 {
 	argc--;
-	if (argc < 2) {
+	if (argc < 2 || argc > 3) {
 		err("Numero de argumentos incorrecto");
 	}
 	
