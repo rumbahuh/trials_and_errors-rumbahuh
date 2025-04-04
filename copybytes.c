81b0a520fad948fd12ad1c226b2bb0560a3a02d0
r cast <157695906+rumbahuh@users.noreply.github.com>
2025-04-04 07:46:47 +0200

    Update copybytes.c
    Argc requirements. To implement path check

--- copybytes.c
+++ copybytes.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib>
+#include <errno.h>
 
+void
+comprobarpaths(int argc, char *argv[])
+{
+	printf("function to check paths");
+}
+
+int
+main(int argc, char *argv[])
+{
+	argc--;
+	if (argc < 2) {
+		err("Numero de argumentos incorrecto");
+	}
+	
+	comprobarpaths(argc, argv);
+	
+	exit(EXIT_SUCCESS);
+}
